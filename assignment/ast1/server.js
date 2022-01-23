10cb445687a63ae69f5b9c7b0279d348b3487983
meashishpokhrel <meashishpokhrel@gmail.com>
2022-01-23 23:32:19 +0545

    Node session

---
+++ assignment/ast1/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname));
+app.get("/", (req, res) => res.sendFile(__dirname + "/form.html"))
+
+
+app.post("/", (req, res) => {
+  res.writeHead(200, { "Content-type": "text/html" })
+  if (req.body.password === "leapfrog"){
+    console.log("success")
+    res.write(`Welcome ${req.body.username} <br> Webpage Designing SOON !!!`)
+  }
+  else{
+      res.write(`WRONG PASSWORD! <br> <b>${req.body.username} </b>, Did you Forgot your password ?`)
+      console.log("wrong")
+  }
+  res.end()
+})
+
+app.listen(port, () => {
+  console.log(`we are on the port ${port}`)
+})
