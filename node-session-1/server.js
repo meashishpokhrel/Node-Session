9d035004f9a07bb03e3581b525fd596d00c70a63
meashishpokhrel <meashishpokhrel@gmail.com>
2022-01-25 10:31:29 +0545

    node session

--- node-session-1/server.js
+++ node-session-1/server.js
@@ -1,32 +1,58 @@
-// const express = require('express')
-// const app = express()
+// // const express = require('express')
+// // const app = express()
 
-// app.get('/', (req, res) => {
-//   res.send('Hi ashish!')
+// // app.get('/', (req, res) => {
+// //   res.send('Hi ashish!')
+// // })
+
+// // app.listen(3000, () => console.log('Server ready'))
+
+// const http = require("http")
+// const dt = require("./dateTime")
+// const url = require("url")
+// const fs = require("fs")
+
+// const port = process.env.PORT || 3000
+
+// const server = http.createServer((req,res)=> {
+//   // const test = url.parse(req.url).query
+//   // console.log(test)
+//   // console.log(req.url)
+//   fs.readFile("writeFile.html", (err, data) => {
+//     res.writeHead(200, { "Content-Type": "text/html" })
+//     res.write(data);
+//     return res.end()
+//   })
+
 // })
 
-// app.listen(3000, () => console.log('Server ready'))
+// server.listen(port, () => {
+//   console.log(dt.myDateTime())
+//   console.log(`Server Runnning on Port ${port}`)
+// })
 
-const http = require("http")
-const dt = require("./dateTime")
-const url = require("url")
-const fs = require("fs")
+const express = require ("express")
+const app = express()
+const router = express.Router()
 
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req,res)=> {
-  // const test = url.parse(req.url).query
-  // console.log(test)
-  // console.log(req.url)
-  fs.readFile("writeFile.html", (err, data) => {
-    res.writeHead(200, { "Content-Type": "text/html" })
-    res.write(data);
-    return res.end()
-  })
+router.get("/login", (req,res) => {
+  res.send("Login Success")
 
 })
 
-server.listen(port, () => {
-  console.log(dt.myDateTime())
-  console.log(`Server Runnning on Port ${port}`)
+router.get("/register", (req,res) => {
+  res.send("Trying to Register")
+  
+})
+
+router.get("/logout", (req,res) => {
+  res.send("Trying to logout !")
+  
+})
+app.use("/", router)
+
+app.listen(port, () => {
+  console.log(`Server runnign at port ${port}`)
 })
