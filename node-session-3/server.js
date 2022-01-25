d68250bbde3fe1af0f3008d1e598ef55c0c40b1f
meashishpokhrel <meashishpokhrel@gmail.com>
2022-01-25 23:03:16 +0545

    node session 3

--- node-session-3/server.js
+++ node-session-3/server.js
@@ -1,75 +1,39 @@
-const express = require("express");
-const fs = require("fs")
-const app = express();
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
+const path = require("path")
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-function test(req, res, next)
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-router.get("/", (req, res) => res.send("Welcome"));
-
-router.get("/login", (req, res) => {
-  console.log(req.params);
-  res.sendFile(__dirname + "/login.html");
-});
-
-router.post("/login", (req, res) => {
-  console.log(req.body);
-  const { email, password } = req.body;
-  if (email === "test@gmail.com" && password === "test") {
-      const data = require("./data.json")
-      console.log("sucess")
-      res.send(data)
-    // res.send({ email, password });
-  } else {
-    res.send("Invalid email or password");
-  }
-});
-
-router.get("/todos", (req, res) => {
-    // res.sendFile(__dirname + "/data.html");
-    var data = fs.readFileSync("data.json")
-    data = JSON.parse(data)
-    // const data = require ("./data.json")
-    // res.status(200).send(data)
-  });
-
-router.post("/todos", (req,res) => {
-    var data = fs.readFileSync("data.json")
-    data = JSON.parse(data)
-    
-
-    data.push({
-        title: req.body.title,
-        description: req.body.description,
-        status: req.body.status
-    })
-    
-    var newData = JSON.stringify(data)
-    fs.writeFile("data.json", newData, (err) => {
-        //err checking
-        if (err) throw err
-
-        console.log("New data added")
-    })
-    res.status(200).send(data)
+router.get("/login", (req,res) => {
+    res.sendFile(path.join(__dirname, "/form.html"))
 })
 
-router.delete("/todos", (req,res) => {
-    const data = require("./data.json")
-    res.status(200).send(data)
+router.post("/login",(req,res) => {
+    let data={}
+    if (req.body.username === "ashish" && req.body.password === "1234"){
+        data = { username: "ashish", friends: ["abc", "xyz"]}
+        res.status(200).send(data)
+        console.log("success")
+    }
+    else{
+        res.send("failure")
+        console.log("failure")
+    }
 })
 
-// router.get("/", (req,res) => {
-//     res.sendFile(path.join(__dirname, "data.html"))
-// })
+router.get ("/register",(req,res) => {
+    res.send("Registered")
+})
 
-
-app.use("/", router);
+router.get ("/logout",(req,res) => {
+    res.send("logout")
+})
+app.use("/", router) //binding with the app,  base url ma bind gareko, base url pachi j aaucha bind garcha
 
 app.listen(port, () => {
-  console.log(`Listening on the port ${port}`);
-});
+    console.log(`Server is listening to the port ${port}`)
+})
